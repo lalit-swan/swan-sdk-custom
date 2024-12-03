93bfaf78e61131439f09c60a489b212f47cb963f
icysharp <lalitr@gmail.com>
2024-12-03 12:45:02 +0530

    feat: add scan upload with webhook callback

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import Swan from '@swan-admin/swan-ai-measurements';
 import dotenv from 'dotenv';
+import crypto from 'crypto';
+import { readFile } from 'fs/promises';
+import { File } from 'buffer';
 
 dotenv.config();
 console.log(process.env.SWAN_API_KEY);
-const swan = new Swan(process.env.SWAN_API_KEY);
+const swan = new Swan(process.env.SWAN_API_KEY, true);
 
 const timestamp = Date.now();
 const testStoreUrl = `https://swantest-store-${timestamp}.com`;
@@ -23,3 +26,30 @@ console.log('Customer Created:', customerCreated.data);
 
 const customerConfig = await swan.custom.getCustomCustomerConfig(testStoreUrl);
 console.log('Customer Config:', customerConfig.data);
+
+const scanId = crypto.randomUUID();
+const arrayMetaData = [
+  { gender: 'male' },
+  { focal_length: '23.57' },
+  { height: '180' },
+  { customer_store_url: testStoreUrl },
+  { clothes_fit: '0' },
+  { scan_type: 'clothing_custom_scan' },
+  { callback_url: process.env.WEBHOOK_URL },
+];
+console.log(`Uploading video file with scanId: ${scanId}`, arrayMetaData);
+
+const video = await readFile('./videos/video.webm');
+const videoFile = new File([video], `${scanId}.webm`, {
+  type: 'video/webm',
+});
+const fileData = {
+  file: videoFile,
+  arrayMetaData,
+  scanId: scanId,
+  email: 'user@example.com',
+};
+
+const uploaded = await swan.fileUpload.uploadFile(fileData);
+console.log(uploaded.message);
+
